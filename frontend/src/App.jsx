29e4551682f7ff2d1e5e5a484fd48085bf26c7f3
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-05-15 16:28:59 +0530

    50 % complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home"
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 
 function App() {
 
