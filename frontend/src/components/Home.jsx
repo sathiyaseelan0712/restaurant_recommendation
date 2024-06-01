7c7eb6fefbbf9231c846531506825ab7ca6785c7
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-06-02 03:53:16 +0530

    frontend added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,22 +11,26 @@ function Home(){
         getRestaraunt();
     },[])   
 
-    return(
+      return (
         <>
-        <div className="flex ">
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 ">
-          {details.map((detail, index) => (
-            <div key={index} className="flex flex-col space-y-4 m-4">
-            <h2>{detail.name}</h2>
-            <p>{detail.location}</p>
-            <p>{detail.address}</p>
-            <p>{detail.contact}</p>
-            <p>{detail.rating}</p>
+        <div className="flex bg-purple-200">
+        <div className="container mx-auto p-4 bg-purple-400 mt-10 ml-25 border-b rounded-lg   mb-10">
+        <div className="flex flex-wrap">
+            {details.map((detail, index) => (
+            <div key={index} className="container font-['Denk_One'] mx-auto p-2 m-4  border-b rounded-lg max-w-md bg-purple-300">
+                <div className="flex flex-col space-y-4">
+                <h2>Name : {detail.name}</h2>
+                <p>Location : {detail.location}</p>
+                <p>Address : {detail.address}</p>
+                <p>Contact : {detail.contact}</p>
+                <p>Rating : {detail.rating}</p>
+                </div>
             </div>
-        ))}
-      </div>
-      </div>
-        </>
-    )
+            ))}
+        </div>
+        </div>
+        </div>
+    </>
+    );
 }
 export default Home;
